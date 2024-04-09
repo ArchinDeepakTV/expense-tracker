0d6c93f2e9708d8edf18b94177aaf6bc827211e4
ArchinDeepakTV <archindeepakad.ad@gmail.com>
2024-04-09 13:50:31 +0530

    added usenavigate

--- expenses/src/components/ExpenseListing.jsx
+++ expenses/src/components/ExpenseListing.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Navbar from "./Navbar";
+import { useNavigate } from "react-router-dom";
 
 export default function ExpenseListing() {
   const [data_from_db, set_data_from_db] = React.useState("");
@@ -36,7 +37,8 @@ export default function ExpenseListing() {
       .then((data) => {
         console.log(data);
       });
-    window.location.href = "/";
+    const navigate = useNavigate();
+    navigate("/expensesList");
   };
 
   return (
